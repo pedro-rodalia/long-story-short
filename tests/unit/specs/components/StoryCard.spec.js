2322c491df5d41f4c399413c5da454bca77a8367
Pedro Rodríguez Alía <pedro.rodalia@gmail.com>
2020-08-05 11:52:04 +0200

    Fix - StoryCard component test suite fixed

--- tests/unit/specs/components/StoryCard.spec.js
+++ tests/unit/specs/components/StoryCard.spec.js
@@ -10,7 +10,7 @@ describe('StoryCard test suite', () => {
     propsData: { story: Stories[0] },
   })
   test('The Story Card component shows the title and an abstract of the content', () => {
-    expect(wrapper.find('h1').text()).toBe(Stories[0].title)
-    expect(wrapper.find('p').text()).toBe(Stories[0].content)
+    expect(wrapper.find('.story-card__title').text()).toBe(Stories[0].title)
+    expect(wrapper.find('.story-card__abstract').text()).toBe(Stories[0].content)
   })
 })
