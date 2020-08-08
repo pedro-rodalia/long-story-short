7e2b0229e8bb60651b51a215afdae8a881e237d6
Pedro Rodríguez Alía <pedro.rodalia@gmail.com>
2020-08-08 16:31:10 +0200

    Missing testing mocked files fixed

--- tests/unit/specs/components/StoryCard.spec.js
+++ tests/unit/specs/components/StoryCard.spec.js
@@ -1,8 +1,15 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import StoryCard from '@/components/story-card/StoryCard'
-import Stories from 'stories'
 
 const localVue = createLocalVue()
+const Stories = [
+  {
+    title: 'A la luz del sol',
+    author: 'Mateo Lopez',
+    abstract:
+      'El repiquetear de las gotas en el cemento se ha convertido en nuestro segundero. Contamos los pasos de la muerte. Su llegada es inevitable y sólo queremos saber cuándo nos dará caza.',
+  },
+]
 
 describe('StoryCard test suite', () => {
   const wrapper = shallowMount(StoryCard, {
